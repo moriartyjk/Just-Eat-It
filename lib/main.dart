1273f1b83d053062dd90c69e2a4de5d37166dd82
Jen <jkmoriarty8@gmail.com>
2022-03-15 14:37:09 -0400

    Solid firebase doc/field access
    By using FutureBuilder to read Document snapshots, the current code can 'randomly' access the documents stored in the restaurants collection.
    The UI will have to change and plan is to move this to a second page that is navigated to from main menu.
    We might have to update how we access these fields in the future, but this works for basic functionalities.

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 import 'dart:math';
 
-Map<String, dynamic> doc = {};
-//CollectionReference<Map<String, dynamic>> col = {};
-//Map<int, QueryDocumentSnapshot<Map<String, dynamic>>> docs = {};
-int randIndex = 0;
+int randIndex = 0; //global index
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,11 +24,7 @@ void main() async {
       print('Wrong password provided for that user.');
     }
   }
-  FirebaseFirestore firestore = FirebaseFirestore.instance;
-  doc = (await firestore.collection("restaurants").get()).docs.first.data();
-  //doc = (await firestore.collection("restaurants").get()).docs.elementAt(randIndex).data();
-  //docs = (await firestore.collection("restaurants").get()).docs.asMap();
-  //doc = (await firestore.collection("restaurants").get()).docs.elementAt(35).data();
+
   runApp(const MyApp());
 }
 
@@ -44,15 +37,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Just Eat It',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green, //figure out better green
       ),
       home: const MyHomePage(title: 'Just Eat It'),
@@ -63,15 +47,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -79,67 +54,55 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //int _counter = 0;
-
-  //int _randIndex = 0;
 
   FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-    /*
-  void _incrementCounter() {
+  void _randGen(){
+    //allows for realtime update of randIndex
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }*/
-
-  void _getRandIndex(){
-    setState(() {
-      randIndex = Random().nextInt(30); //random number [0, 37]
-      //print(randIndex);
-      _getRestaurant();
+      randIndex = Random().nextInt(37); // random num [0, 37)
     });
   }
-
-  void _getRestaurant() async{
-    doc = (await firestore.collection("restaurants").get()).docs.elementAt(randIndex).data(); 
-  }
-
   @override
   Widget build(BuildContext context) {
 
-    String name = doc["name"];
-    //String location = doc["location"];
+    CollectionReference restaurants = FirebaseFirestore.instance.collection("restaurants");
 
+    String docID = randIndex.toString(); //cast index to string because documents are stored by "numerical" indexes
+    //print("Document Id: $docID");
+  
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
+        //This is based on implementation from Flutter docs in https://firebase.flutter.dev/docs/firestore/usage/
+        child: FutureBuilder<DocumentSnapshot>(
+          future: restaurants.doc(docID).get(),
+          builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if(snapshot.hasError){
+              return const Text("Something went wrong");
+            }
 
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$randIndex',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text("Name: $name"),
-            //Text("Location: $location"),
-          ],
+            if(snapshot.hasData && !snapshot.data!.exists){
+              return const Text("Document does not exist",);
+            }
+
+            if(snapshot.connectionState == ConnectionState.done) {
+              Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+              return Text(
+                "Document Id: $docID\nName: ${data["name"]}", 
+                style: const TextStyle(fontSize: 40),
+              );
+            }
+            return const Text("loading"); //appears while firestore is retrieving data
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _getRandIndex, //trigger random number generation
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _randGen, //trigger random number generation
+        tooltip: 'Retry',
+        child: const Icon(Icons.refresh),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
